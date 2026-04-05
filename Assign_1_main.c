ed304775d6896b6efc7217c6e1e1b5d4c2edd077
NissimBansal <aryan.nissim@gmail.com>
2026-04-05 22:11:33 +0530

    Updated C code

--- Assign_1_main.c
+++ Assign_1_main.c
@@ -4,6 +4,8 @@
 #define AHB1_GPIOA 0x40020000
 #define APB1_TIM2 0x40000000
 
+void TIM2_IRQHandler(void);
+
 int main(void)
 {
 	*((uint32_t*)(AHB1_RCC + 0x30)) |= (1 << 0); /* enabling clock for GPIOA */
@@ -15,15 +17,15 @@ int main(void)
 	*((uint32_t*)(APB1_TIM2 + 0x0C)) |= (1 << 0); /* enabling DIER / interrupt channel */
 	*((volatile uint32_t*)(0xE000E100)) |= (1 << 28); /*  NVIC now responds to TIM2_DIER changes*/
 	*((uint32_t*)(APB1_TIM2)) |= (1 << 0); /* starts counter */
-	
+
 	while(1)
 	{
 		__asm volatile ("wfi"); /* assembly code that makes cpu sleep until a flag is passed */
 	}
-	
-	void TIM2_IRQHandler(void);
-	{
-		*((uint32_t*)(APB1_TIM2 + 0x10)) &= ~(1 << 0); /* resets the flag */
-		*((uint32_t*)(AHB1_GPIOA + 0x14)) ^= (1 << 5); /* toggles LED between ON & OFF */
-	}
+}
+
+void TIM2_IRQHandler(void)
+{
+	*((uint32_t*)(APB1_TIM2 + 0x10)) &= ~(1 << 0); /* resets the flag */
+	*((uint32_t*)(AHB1_GPIOA + 0x14)) ^= (1 << 5); /* toggles LED between ON & OFF */
 }
