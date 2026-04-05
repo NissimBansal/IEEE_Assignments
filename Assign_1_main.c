5d7b6de1426999539fbe0496c33b694a42e73fc4
NissimBansal <aryan.nissim@gmail.com>
2026-04-05 22:05:07 +0530

    Updated C code

--- Assign_1_main.c
+++ Assign_1_main.c
@@ -8,8 +8,22 @@ int main(void)
 {
 	*((uint32_t*)(AHB1_RCC + 0x30)) |= (1 << 0); /* enabling clock for GPIOA */
 	*((uint32_t*)(AHB1_RCC + 0x40)) |= (1 << 0); /* enabling clock for TIM2 */
-	*((uint32_t*)(AHB1_GPIOA)) |= (1 << 10); /* setting MODER5 to output part 1 */
-	*((uint32_t*)(AHB1_GPIOA)) &= ~(1 << 11); /* setting MODER5 to output part 2*/
+	*((uint32_t*)(AHB1_GPIOA)) |= (1 << 10); /* setting MODER5 to output */
+	*((uint32_t*)(AHB1_GPIOA)) &= ~(1 << 11); /* setting MODER5 to output */
 	*((uint32_t*)(APB1_TIM2 + 0x28)) = 8399; /* setting prescalar */
 	*((uint32_t*)(APB1_TIM2 + 0x2C)) = 19999; /* setting auto-reload */
+	*((uint32_t*)(APB1_TIM2 + 0x0C)) |= (1 << 0); /* enabling DIER / interrupt channel */
+	*((volatile uint32_t*)(0xE000E100)) |= (1 << 28); /*  NVIC now responds to TIM2_DIER changes*/
+	*((uint32_t*)(APB1_TIM2)) |= (1 << 0); /* starts counter */
+	
+	while(1)
+	{
+		__asm volatile ("wfi"); /* assembly code that makes cpu sleep until a flag is passed */
+	}
+	
+	void TIM2_IRQHandler(void);
+	{
+		*((uint32_t*)(APB1_TIM2 + 0x10)) &= ~(1 << 0); /* resets the flag */
+		*((uint32_t*)(AHB1_GPIOA + 0x14)) ^= (1 << 5); /* toggles LED between ON & OFF */
+	}
 }
